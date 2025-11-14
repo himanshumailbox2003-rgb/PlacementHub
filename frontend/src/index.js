ae2023de6528e30dc23d67076524e74c6625c0c2
himanshumailbox2003-rgb <himanshumailbox2003@gmail.com>
2025-11-14 22:53:37 +0530

    Import axios and configure base URL
    Import axios and set base URL from environment variable.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,9 @@
-import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import axios from "axios";
 
-// Set base URL from environment variable
 axios.defaults.baseURL = process.env.REACT_APP_API;
-console.log("Backend URL => ", axios.defaults.baseURL);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
