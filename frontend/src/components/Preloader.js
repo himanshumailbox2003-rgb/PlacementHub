ae3863d5fb43692254052f9a51686075b95f38d3
himanshumailbox2003-rgb <himanshumailbox2003@gmail.com>
2025-11-15 14:26:21 +0530

    Create Preloader.js

---
+++ frontend/src/components/Preloader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Preloader() {
+  return (
+    <div id="ph-preloader" className="preloader">
+      <div className="spinner" />
+    </div>
+  );
+}
