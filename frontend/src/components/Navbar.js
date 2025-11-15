74b7127f7363a108dca680af0866193f389e7f2b
himanshumailbox2003-rgb <himanshumailbox2003@gmail.com>
2025-11-15 14:24:00 +0530

    Create Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ThemeToggle from "./ThemeToggle";
+import "./navbar.css";
+
+export default function Navbar() {
+  return (
+    <header className="nav">
+      <div className="nav-inner">
+        <div className="brand">
+          <Link to="/" className="brand-link">PlacementHub</Link>
+        </div>
+
+        <nav className="nav-right">
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/login" className="nav-link">Login</Link>
+          <Link to="/register" className="nav-link">Register</Link>
+          <ThemeToggle />
+        </nav>
+      </div>
+    </header>
+  );
+}
